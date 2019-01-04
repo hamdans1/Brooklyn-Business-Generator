4117341f3bb00c39166985fb03ae450a4fa261bc
Sami Hamdan <samihamdan00@gmail.com>
2019-01-04 17:45:10 -0500

    updated styling and finsihed business generator function

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let businessName = '';
+
 const neighborhoodArray = [
   'Clinton Hill',
   'Cobble Hill',
@@ -13,7 +15,7 @@ const descriptorArray = [
   'Organic',
   'Artisan',
   'Craft',
-  'Sweet',
+  'Fair Trade',
   'Handmade',
   'Vegan'
 ];
@@ -40,8 +42,11 @@ const businessArray = [
 
 //Clicking 'generate' button should call function that returns random element from each array
 
-const businessGenerator() {
-  let businessName = '';
-  businessName = neighborhoodArray[Math.floor(Math.random * neighborhoodArray.length)];
+const businessGenerator = () => {
+  let randomNeighborhood = neighborhoodArray[Math.floor(Math.random() * neighborhoodArray.length)];
+  let randomDescriptor = descriptorArray[Math.floor(Math.random() * descriptorArray.length)];
+  let randomProduct = productArray[Math.floor(Math.random() * productArray.length)];
+  let randomBusiness = businessArray[Math.floor(Math.random() * businessArray.length)];
+  businessName = `${randomNeighborhood} ${randomDescriptor} ${randomProduct} ${randomBusiness}`;
   return businessName;
-}
+};
